213d20361c43d95b557c02b86927692f22a51f5d
Manu <korben@korben.info>
2025-04-21 10:51:29 +0200

    env sample

--- utils/providers.js
+++ utils/providers.js
@@ -1,19 +1,9 @@
 // Modèles Ollama disponibles
 export const PROVIDERS = {
-  "deepseek-r1:7b": {
-    name: "DeepSeek 7B",
-    max_tokens: 131_000,
-    id: "deepseek-r1:7b",
-  },
   "deepseek-r1:70b": {
     name: "DeepSeek 70B",
     max_tokens: 131_000,
     id: "deepseek-r1:70b",
-  },
-  "llama3.1:8b": {
-    name: "Llama 3.1 8B",
-    max_tokens: 131_000,
-    id: "llama3.1:8b",
   },
   "gemma3:27b": {
     name: "Gemma 3 27B",
@@ -29,5 +19,10 @@ export const PROVIDERS = {
     name: "DeepCoder",
     max_tokens: 131_000,
     id: "deepcoder:14b",
+  },
+  "cogito:70b": {
+    name: "Cogito 70B",
+    max_tokens: 131_000,
+    id: "cogito:70b",
   },
 };
